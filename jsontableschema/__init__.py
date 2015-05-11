dfffaa946a6760073276d1f759d6629f9093f86e
Dragon Dave McKee <dragon@scraperwiki.com>
2015-05-11 12:02:48 +0100

    py3 compatibility

--- jsontableschema/__init__.py
+++ jsontableschema/__init__.py
@@ -8,6 +8,7 @@
 # it under the terms of the Apache Software Licence v2.0
 
 import json
+unicode = type(u'')  # PY3 compatibility
 
 class FormatError(Exception): pass
 class DuplicateFieldId(Exception): pass
@@ -49,10 +50,10 @@ class JSONTableSchema(object):
         self.format_version = self.__format_version__
         if json_string is not None:
             self.of_string(json_string)
-        
+
     def of_string(self, s):
         self.of_json(json.loads(s))
-        
+
     def of_json(self, j):
         if "fields" not in j:
             raise FormatError("Field `fields' must be present in dictionary")
@@ -70,8 +71,8 @@ class JSONTableSchema(object):
                     err_tmpl = "Field descriptor %d must contain key `%s'"
                     raise FormatError(err_tmpl % (idx, k))
 
-            self.add_field(field_id=stanza["id"], 
-                           label=stanza["label"], 
+            self.add_field(field_id=stanza["id"],
+                           label=stanza["label"],
                            field_type=stanza["type"])
 
         self.format_version = j.get("json_table_schema_version",
@@ -80,7 +81,7 @@ class JSONTableSchema(object):
     @property
     def field_ids(self):
         return [ i["id"] for i in self.fields ]
-            
+
     def add_field(self, field_id=None, label=None, field_type=None):
         if not isinstance(field_id, (str, unicode)):
             raise FormatError("Field `id' must be a string")
@@ -91,7 +92,7 @@ class JSONTableSchema(object):
 
         if field_id in self.field_ids:
             raise DuplicateFieldId("field_id")
-            
+
         if field_type not in self.__valid_type_names__:
             err_tmpl = "Invalid type `%s' in field descriptor for `%s'"
             raise FormatError(err_tmpl % (field_type, label))
@@ -101,7 +102,7 @@ class JSONTableSchema(object):
                 "label": label,
                 "type": field_type
                 })
-        
+
     def remove_field(self, field_id):
         if field_id not in self.field_ids:
             raise KeyError
